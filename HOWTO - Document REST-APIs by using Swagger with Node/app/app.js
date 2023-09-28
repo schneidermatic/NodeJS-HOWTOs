d4a47b58d1024189c89a9581acc4c9aea463a4fb
schneidermatic <markus.schneider73@gmail.com>
2023-09-28 18:48:54 +0200

    * Added - Initial commit of 'HOWTO - Document REST-APIs by using Swagger with Node'.
     On branch main
     Your branch is ahead of 'origin/main' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	new file:   HOWTO - Document REST-APIs by using Swagger with Node/.gitignore
    	new file:   HOWTO - Document REST-APIs by using Swagger with Node/app/app.js
    	new file:   HOWTO - Document REST-APIs by using Swagger with Node/app/package-lock.json
    	new file:   HOWTO - Document REST-APIs by using Swagger with Node/app/package.json

---
+++ HOWTO - Document REST-APIs by using Swagger with Node/app/app.js
@@ -0,0 +1,62 @@
+
+const express = require('express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+const app = express();
+const port = 3000;
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Library API',
+            version: '1.0.0'
+        }
+    },
+    apis: ['app.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+console.log(swaggerDocs);
+
+app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     description: Get all books
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+app.get('/books', (req, res) => {
+    res.send([
+        {
+            id: 1,
+            title: 'Harry Potter'
+        }
+
+    ])
+});
+
+/**
+ * @swagger
+ * /books:
+ *   post:
+ *     description: Create a new book
+ *     parameters:
+ *     - name: title
+ *       description: title of the book
+ *       in: formData
+ *       required: true
+ *       type: string
+ *     responses:
+ *       201:
+ *         description: Created
+ */
+app.post('/books', (req, res) => {
+    res.status(201).send();
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
